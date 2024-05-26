07dc04c4138c0842bff40a672ab757a8a6e627c9
FatimahHabib84 <fatimahhabib84@gmail.com>
2024-05-26 19:10:19 +0300

    lap-1 D-1 W-5

---
+++ main.js
@@ -0,0 +1,38 @@
+let h1Class = document.getElementsByClassName('h1Class')[0];
+console.log(h1Class)
+h1Class.style.color = "white";
+h1Class.style.backgroundColor = "lightblue"
+
+// let pID = document.getElementById('pID')
+let head = document.getElementsByTagName('HEAD')[0]
+let linkForP = document.createElement('link')
+linkForP.rel = 'stylesheet'
+linkForP.type = 'text/css'
+linkForP.href = 'style.css'
+head.appendChild(linkForP)
+
+img = document.getElementsByTagName('img')[0]
+img.src = 'https://cdn-img1.imgworlds.com/assets/a5366382-0c26-4726-9873-45d69d24f819.jpg?key=home-gallery'
+
+ul = document.querySelector('ul')
+for(let i=0 ; i<3 ; i++){
+    let li = document.createElement(`li`)
+    ul.appendChild(li)
+    li.innerText = `${i}`
+    li.style.display = 'inline'
+    li.style.color = 'blue'
+    li.style.border = '1px solid black'
+}
+body = document.getElementsByTagName(`body`)[0]
+table = document.createElement('table')
+table.setAttribute('class','table')
+body.appendChild(table)
+for(let i=0 ; i<2 ; i++){
+    let tr = document.createElement('tr')
+    table.appendChild(tr)
+    for(let j=0 ; j<3 ; j++){
+        let td = document.createElement('td')
+        tr.appendChild(td)
+        td.innerText = `cell [${i}${j}]`
+    }
+}
